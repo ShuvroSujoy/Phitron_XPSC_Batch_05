42cf394ab7853b1b518f29dc43e9e72f01c5e232
Sujoy Kumar Ghosh <143160284+ShuvroSujoy@users.noreply.github.com>
2024-11-03 17:58:24 +0600

    Add files via upload
    Week 2 Day 1 Has Uploaded

---
+++ Week_2/Day_1/B_Minor_Change.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s, t;
+    cin >> s >> t;
+    int len = s.size();
+    int cnt = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] != t[i])
+        {
+            cnt++;
+        }
+    }
+    cout<<cnt<<'\n';
+
+    return 0;
+}
